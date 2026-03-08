ba9d8950dd623393c7a1daf6b43540de464eaff1
HARSHITH CHINTAKINDI <chintakindibhargav2016@gmail.com>
2026-03-08 09:31:52 +0530

    Model Trained

---
+++ src/model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import joblib
+import os
+
+
+# In[9]:
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+os.makedirs("models", exist_ok=True)
+
+def train_model(inputs,target):
+    inputs_train, inputs_test, target_train, target_test=train_test_split(inputs, target, test_size=0.2, random_state=42)
+    model=RandomForestClassifier(n_estimators=200, random_state=42)
+    model.fit(inputs_train, target_train)
+    accuracy=model.score(inputs_test, target_test)
+    print("Baseline Accuracy:", accuracy)
+    joblib.dump(model, "models/model.pkl")
+    return model, inputs_train, accuracy
+
+
+# In[10]:
+
+
+# inputs=pd.read_parquet('inputs.parquet')
+# target=pd.read_parquet('target.parquet').squeeze()
+# train_model(inputs,target)
+
+
+# In[ ]:
+
+
+
+
